bbdda4c42484636c9286faa1898c45bdd64af983
matildasoderhall <soderhall.matilda@gmail.com>
2024-12-20 17:04:54 +0100

    fix deploy error temporarly

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import questions from './modules/questions';
 // --------------------------------- VARIABLES ---------------------------------
 // -----------------------------------------------------------------------------
 
-let currentScore = 0; 
-let currentTimeInTimer = 0; 
+// prepared variables for future use, they are currently commented out until they are used
+// let currentScore = 0; 
+// let currentTimeInTimer = 0; 
 
-
+// temporary solution to error for not using variable
+console.log(questions);
