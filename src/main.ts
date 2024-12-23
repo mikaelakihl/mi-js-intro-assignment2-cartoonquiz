0185f73aea956177f7b81835761534e5f6873942
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2024-12-23 11:41:24 +0100

    add clickevent for play-game-btn

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import questions from './modules/questions';
 // --------------------------------- VARIABLES ---------------------------------
 // -----------------------------------------------------------------------------
 
+const playGameBtn  = document.querySelector('#playGameBtn');
+
+playGameBtn!.addEventListener('click', playGame);
+
+console.log(playGameBtn);
+
+function playGame(){
+    
+}
+
 // prepared variables for future use, they are currently commented out until they are used
 // let currentScore = 0; 
 // let currentTimeInTimer = 0; 
