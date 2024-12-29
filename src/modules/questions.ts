319625f392a6f826ca0c50bfcb1f21b319f40326
NicoleSilfverling <92078589+NicoleSilfverling@users.noreply.github.com>
2024-12-29 23:21:12 +0100

    change questions and answers

--- src/modules/questions.ts
+++ src/modules/questions.ts
@@ -14,108 +14,106 @@ interface IQuestion {
 const questions: IQuestion[] = [
   {
     id: 0,
+    question: 'What is the name of SpongeBob SquarePants best friend?',
+    correctAnswer: 'Patrick Star',
+    incorrectAnswers: ['Squidward Tentacles', 'Mr. Krabs'],
+  },
+  {
+    id: 1,
+    question: 'Which city does Batman protect in the animated series?',
+    correctAnswer: 'Gotham City',
+    incorrectAnswers: ['Metropolis', 'Star City'],
+  },
+  {
+    id: 2,
+    question: 'In Pokémon, what is the evolved form of Pikachu?',
+    correctAnswer: 'Raichu',
+    incorrectAnswers: ['Pichu', 'Jolteon'],
+  },
+  {
+    id: 3,
+    question:
+      "What is the name of Dexter's older sister in 'Dexter's Laboratory'?",
+    correctAnswer: 'Dee Dee',
+    incorrectAnswers: ['Lily', 'Mandy'],
+  },
+  {
+    id: 4,
     question:
       'What is lost in Hawaiian and is also the name of a little girl in a 2002 film which features a alien named Stitch?',
     correctAnswer: 'Lilo',
     incorrectAnswers: ['Lolo', 'Lucy'],
   },
   {
-    id: 1,
-    question:
-      'In the Star Wars universe, what species is Grand Admiral Thrawn?',
-    correctAnswer: 'Chiss',
-    incorrectAnswers: ['Gungans', 'Pantorans'],
-  },
-  {
-    id: 2,
-    question:
-      "In SpongeBob SquarePants, what is the name of Sandy Cheek's place of residence?",
-    correctAnswer: "Sandy's Treedome",
-    incorrectAnswers: ["Sandy's Bubble", 'Auquatic Reseach Centre'],
-  },
-  {
-    id: 3,
-    question:
-      'In Rick and Morty from which dimension do Rick and Morty originate from?',
-    correctAnswer: 'C-137',
-    incorrectAnswers: ['J1977', 'C-136'],
-  },
-  {
-    id: 4,
-    question:
-      'Which episode from The Amazing World Of Gumball won the Childrens Choice Award at the British Animation Awards in 2016?',
-    correctAnswer: 'The Shell',
-    incorrectAnswers: ['The Limit', 'The Kids'],
-  },
-  {
     id: 5,
     question:
-      'The song Little April Shower features in which Disney cartoon film?',
-    correctAnswer: 'Bambi',
-    incorrectAnswers: ['Cinderella', 'Pinocchio'],
+      "Who is the villain in 'The Powerpuff Girls' known for his red skin and sinister demeanor?",
+    correctAnswer: 'Him',
+    incorrectAnswers: ['Dr. X', 'Professor Utonium'],
   },
   {
     id: 6,
-    question: "What is Dipper's real name from Gravity Falls?",
-    correctAnswer: 'Mason Pines',
-    incorrectAnswers: ['Mable Pines', 'Jason Pines'],
+    question:
+      'In Avatar: The Last Airbender, what element does Aang master last?',
+    correctAnswer: 'Fire',
+    incorrectAnswers: ['Earth', 'Water'],
   },
   {
     id: 7,
-    question: 'What year was the Disney film A Goofy Movie released?',
-    correctAnswer: '1995',
-    incorrectAnswers: ['2001', '1997'],
+    question: "What is the name of Ash Ketchum's hometown?",
+    correctAnswer: 'Pallet Town',
+    incorrectAnswers: ['Cerulean City', 'Viridian City'],
   },
   {
     id: 8,
-    question: 'What was the first Disney movie to use CGI?',
-    correctAnswer: 'The Black Cauldron',
-    incorrectAnswers: ['Tron', 'Fantasia'],
+    question:
+      "In Tom and Jerry, who often gets hurt as a result of Tom's antics?",
+    correctAnswer: 'Tom',
+    incorrectAnswers: ['Jerry', 'Spike'],
   },
   {
     id: 9,
     question:
-      'Butters Stotch, Pip Pirrup, and Wendy Testaburger are all characters in which long running animated TV series?',
-    correctAnswer: 'South Park',
-    incorrectAnswers: ['The Simpsons', 'Family Guy'],
+      "In 'Adventure Time,' who is Finn's best friend and adoptive brother?",
+    correctAnswer: 'Jake the Dog',
+    incorrectAnswers: ['Marceline', 'BMO'],
   },
   {
     id: 10,
-    question:
-      'The stop motion comedy show Robot Chicken was created by which of the following?',
-    correctAnswer: 'Seth Green',
-    incorrectAnswers: ['Seth MacFarlane', 'Seth Rogen'],
+    question: "In Futurama, who is Fry's one-eyed love interest?",
+    correctAnswer: 'Leela',
+    incorrectAnswers: ['Amy', 'Linda'],
   },
   {
     id: 11,
-    question:
-      'Wendy O. Koopa appeared in the Super Mario DIC Cartoons, but what was she known as?',
-    correctAnswer: 'Kootie Pie',
-    incorrectAnswers: ['Sweetie Pie', 'Wendy Pie'],
+    question: "In 'South Park,' what is Kenny known for?",
+    correctAnswer: 'Always dying in episodes',
+    incorrectAnswers: ['Being the smartest kid', 'Inventing schemes'],
   },
   {
     id: 12,
-    question: 'Which Family Guy character got his own spin-off show in 2009?',
-    correctAnswer: 'Cleveland Brown',
-    incorrectAnswers: ['Glenn Quagmire', 'Joe Swanson'],
+    question:
+      "In 'Courage the Cowardly Dog,' where do Courage and his owners live?",
+    correctAnswer: 'Nowhere',
+    incorrectAnswers: ['Somewhere', 'Middle-of-Nowhere'],
   },
   {
     id: 13,
-    question: 'How many episodes were in season five of Samurai Jack?',
-    correctAnswer: '10',
-    incorrectAnswers: ['12', '11'],
+    question: 'In The Smurfs, who is the villain trying to capture the Smurfs?',
+    correctAnswer: 'Gargamel',
+    incorrectAnswers: ['Azrael', 'Brainy'],
   },
   {
     id: 14,
-    question: "What is the name of Ruby Rose's weapon from RWBY?",
-    correctAnswer: 'Crescent Rose',
-    incorrectAnswers: ['Thorned Rosebud', 'Magnhild'],
+    question: "In The Flintstones, what is the name of Fred's wife?",
+    correctAnswer: 'Wilma',
+    incorrectAnswers: ['Betty', 'Pebbles'],
   },
   {
     id: 15,
-    question: 'Which city did Anger berate for ruining pizza in Inside Out?',
-    correctAnswer: 'San Francisco',
-    incorrectAnswers: ['Minnesota', 'California'],
+    question: "Who is the main villain in 'Kim Possible'?",
+    correctAnswer: 'Dr. Drakken',
+    incorrectAnswers: ['Shego', 'Monkey Fist'],
   },
   {
     id: 16,
@@ -126,22 +124,22 @@ const questions: IQuestion[] = [
   {
     id: 17,
     question:
-      "What was Maggie Simpson's first canonical word, not including the Tracey Ullman shorts?",
-    correctAnswer: 'Daddy.',
-    incorrectAnswers: ['Sequel?', 'Rusty!'],
+      'In The Simpsons, what does Homer often say when something goes wrong?',
+    correctAnswer: "D'oh!",
+    incorrectAnswers: ['Woo-hoo!', 'Mmm... donuts!'],
   },
   {
     id: 18,
     question:
-      'What Cartoon Network show aired its first episode on November 4th, 2013?',
-    correctAnswer: 'Steven Universe',
-    incorrectAnswers: ['Adventure Time', 'The Amazing World of Gumball'],
+      'In Rick and Morty, what does Rick often use to travel between dimensions?',
+    correctAnswer: 'The Portal Gun',
+    incorrectAnswers: ['The Time Box', 'The Dimensional Key'],
   },
   {
     id: 19,
-    question: 'Nickelodeon is owned by what parent company?',
-    correctAnswer: 'Viacom',
-    incorrectAnswers: ['CBS', 'FOX'],
+    question: "What is the name of the villain in 'The Lion King'?",
+    correctAnswer: 'Scar',
+    incorrectAnswers: ['Mufasa', 'Zazu'],
   },
 ];
 
